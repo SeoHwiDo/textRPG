d6859f20eb03a4ea56e6f70fcdc43ce48f454513
shd <masterseo01@naver.com>
2026-07-22 16:49:24 +0900

    [mod]주석 추가 및 중복선언 제거

--- textRPG/Status.cpp
+++ textRPG/Status.cpp
@@ -1,15 +1,23 @@
 #include"Status.h"
+// 모든 status의 수치 get
+std::vector<int> Status::getStatusData() const {
+	return this->statusData;
+};
 //status명을 통해 해당 status의 수치 get
 int Status::getStatusData(int _stat) const {
 	return this->statusData[_stat];
 };
+
 //status명을 통해 해당 status의 수치를 amount만큼 set
 
-void Status::setStatusData(int _stat, int amount,bool add=false) {
+void Status::setStatusData(int _stat, int amount,bool add) {
 	if (add) {
 		this->statusData[_stat] += amount;
 	}
 	else {
 		this->statusData[_stat] = amount;
 	}
-};
+}
+//void Status::setUpStatus(statusName _stats[], int size) {
+//	
+//}
--- textRPG/Status.h
+++ textRPG/Status.h
@@ -9,19 +9,21 @@ public:
 		con,
 		wis,
 		cham,
-		remain
+		remain//남은 포인트
 	};
-	const int STATS=6;
-	const int STAT_MIN = 0;
-	const int STAT_MAX = 25;
+	const int STATS=6;//status 종류 수
+	const int STAT_MIN = 0;//status의 최소값
+	const int STAT_MAX = 25;//status의 최대값
 private:
-	std::vector<int> statusData;
+	std::vector<int> statusData;//각 status 별 수치
 
 public:
 	//status명을 통해 해당 status의 수치 get
+	std::vector<int> getStatusData() const;//각 status 별 수치
 	int getStatusData(int _stat) const;
+
 	//status명을 통해 해당 status의 수치를 amount만큼 set, add에 true 전달시 +연산
 	void setStatusData(int _stat, int amount,bool add=false);
-
-	//
+	//status세팅 로직. 강화할 status목록,강화할 status 개수
+	//void setUpStatus(statusName _stats[], int size); ***출력 방식을 정할때까지 보류
 };
