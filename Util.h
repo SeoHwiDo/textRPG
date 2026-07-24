2f0d34c9d702ce443b42494d2476f99a8f636d83
user <user@DESKTOP-CTA5QIP>
2026-07-24 16:58:28 +0900

    [mod] namespace로 묶음
    멤버변수가 없는 경우 불필요한 인스턴스화 방지와 접근제어자를 제거할 수 있는 namespace로 묶는것이 일반적

--- Util.h
+++ Util.h
@@ -1,5 +1,8 @@
 #pragma once
-class Util {
-public:
-	
+#include <random>
+namespace Util {
+
+ bool check_versus_success(double my_stat, double enemy_stat);
+ bool check_success(double stat);
 };
+
