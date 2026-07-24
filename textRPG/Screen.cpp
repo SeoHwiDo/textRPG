4cde792254f7bca306799d9ddf1eeec774c25f12
user <user@DESKTOP-CTA5QIP>
2026-07-24 11:38:05 +0900

    [mod]플레이어 초기화 화면 출력 구현

--- textRPG/Screen.cpp
+++ textRPG/Screen.cpp
@@ -17,34 +17,38 @@ bool Screen::clear_input(bool check) {//입력 허용범위 검사
 
 Player* Screen::initPlayer() {
     std::string name;
-    Status _playerStatus;
     while (1) {
         inOutput("이름을 입력하세요(6글자 이내):", name);
         if(clear_input(name.size()<=6)) break;
         std::cout << "\n6글자를 초과하였습니다!";
     }
-    int remainPoint=Status::DSTAT;
-    for (int stat = 0; stat < Status::STATS; ++stat) {
-        int point;
+    Player* player = new Player(name);
+
+    int point;
+    Status::stat;
+    for (Status::statusType s : Status::stat) {
+        if (s == Status::REMAIN) break;
+        int remainPoint = player->status.getStatusRemain();
         while (1) {
-            std::cout << "\n잔여포인트: " << remainPoint<<"\n "<<Status::statsStr[stat];
+            std::cout << "\n잔여포인트: " << remainPoint << "\n " << Status::statName[s];
             inOutput("\n스탯을 강화할 포인트를 입력하세요:", point);
-            if (clear_input(point<=remainPoint&&point>=0)) break;
-            std::cout << "입력값 및 잔여 포인트를 확인하세요";
+            system("cls");
+            if (clear_input(point <= remainPoint && point >= 0)) break;
+            std::cout << "\n입력값 및 잔여 포인트를 확인하세요";
         }
-        _playerStatus.setStatusData(stat, point);
-        remainPoint -= point;
+        player->status.setStatus(s, Status::validStatValue(player->status.getStatus(s) + point));
+        player->status.setStatusRemain(remainPoint - point);
     }
-    Player *_player=new Player(name, _playerStatus);
-    return _player;
+    return player;
+
 }
 
-void Screen::topInfo(const Player& _player) {
-    std::string name = _player.getName();
-    std::string lv = _player.getLv() + "";
-    std::string exp = _player.getExp() + "";
-    
-}
+//void Screen::topInfo(const Player& _player) {
+//    std::string name = _player.getName();
+//    std::string lv = _player.getLv() + "";
+//    std::string exp = _player.getExp() + "";
+//    
+//}
 
 //std::string Screen::midInfo(Event _event, Choice _choice);
 //std::string Screen::botInfo(const Actor& _actor);
