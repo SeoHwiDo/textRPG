ee3881b0471d864029e5df9d27a48a9610722619
user <user@DESKTOP-CTA5QIP>
2026-07-24 11:36:46 +0900

    [mod]status 클래스의 getter및 setter를 enum타입의 값으로 호출 구현

--- textRPG/Status.cpp
+++ textRPG/Status.cpp
@@ -1,4 +1,47 @@
 ﻿#include"Status.h"
+
+const Status::statusType Status::stat[STATS] = { STR, DEX, CON, WIS, CHAM, REMAIN };
+const std::string Status::statName[STATS] = { "STR", "DEX", "CON", "WIS", "CHAM", "REMAIN" };
+
+
+int Status::getStatusStr() const {
+	return this->status.str;
+}
+int Status::getStatusDex() const {
+	return this->status.dex;
+}
+int Status::getStatusCon() const {
+	return this->status.con;
+}
+int Status::getStatusWis() const {
+	return this->status.wis;
+}
+int Status::getStatusCham() const {
+	return this->status.cham;
+}
+int Status::getStatusRemain() const {
+	return this->status.remain;
+}
+
+int Status::getStatus(int stat) const {
+	switch (stat) {
+	case STR:
+		return this->status.str;
+	case DEX:
+		return this->status.dex;
+	case CON:
+		return this->status.con;
+	case WIS:
+		return this->status.wis;
+	case CHAM:
+		return this->status.cham;
+	case REMAIN:
+		return this->status.remain;
+	default:
+		return -1;
+	}
+}
+
 void Status::setStatusStr(int amount) {
 	this->status.str = amount;
 }
@@ -18,3 +61,45 @@ void Status::setStatusRemain(int amount) {
 	this->status.remain = amount;
 }
 
+void Status::setStatus(int stat, int amount) {
+	switch (stat) {
+	case STR:
+		setStatusStr(amount);
+		break;
+	case DEX:
+		setStatusDex(amount);
+		break;
+	case CON:
+		setStatusCon(amount);
+		break;
+	case WIS:
+		setStatusWis(amount);
+		break;
+	case CHAM:
+		setStatusCham(amount);
+		break;
+	case REMAIN:
+		setStatusRemain(amount);
+		break;
+	default:
+		break;
+	}
+}
+
+void Status::setStatus(int stst[STATS]) {
+	for (int i = 0; i < STATS; i++) {
+		setStatus(i, stst[i]);
+	}
+}
+
+int Status::validStatValue(int stat) {
+	if (stat < STAT_MIN) {
+		return STAT_MIN;
+	}
+	else if (stat > STAT_MAX) {
+		return STAT_MAX;
+	}
+	else {
+		return stat;
+	}
+}
--- textRPG/Status.h
+++ textRPG/Status.h
@@ -3,36 +3,48 @@
 #include<string>
 class Status {
 public:
-	const static int STATS=6;//status 종류 수
+	const static int STATS = 6;//status 종류 수
 	const static int STAT_MIN = 0;//status의 최소값
 	const static int STAT_MAX = 25;//status의 최대값
 	const static int DSTAT = 10;//최초 스탯포인트 디폴트값
+	enum statusType { STR, DEX, CON, WIS, CHAM, REMAIN };//status 종류
+	const static statusType stat[STATS];//status 종류 배열
+	const static std::string statName[STATS];//status 종류 이름 배열
 	struct statusData {
 		int str = 0;
 		int dex = 0;
 		int con = 0;
 		int wis = 0;
 		int cham = 0;
-		int remain = 0;
+		int remain = DSTAT;
 	};
 
 private:
 	//actor객체가 가지고 있는 statusData 구조체
 	statusData status;
 public:
-	//getter
-	int getStatusStr() const { return this->status.str; }
-	int getStatusDex() const { return this->status.dex; }
-	int getStatusCon() const { return this->status.con; }
-	int getStatusWis() const { return this->status.wis; }
-	int getStatusCham() const { return this->status.cham; }
-	int getStatusRemain() const { return this->status.remain; }
+	//특정 status값을 가져오는 getter
+	int getStatusStr() const ;
+	int getStatusDex() const ;
+	int getStatusCon() const ;
+	int getStatusWis() const ;
+	int getStatusCham() const ;
+	int getStatusRemain() const ;
+	//스테이터스 종류를 입력받아 해당 status값을 가져오는 getter
+	int getStatus(int stat) const;
 
-	//setter
+	//특정 status값을 설정하는 setter
 	void setStatusStr(int amount);
-	void setStatusDex(int amount);	
+	void setStatusDex(int amount);
 	void setStatusCon(int amount);
 	void setStatusWis(int amount);
 	void setStatusCham(int amount);
 	void setStatusRemain(int amount);
+
+	//스테이터스 종류를 입력받아 해당 status값을 설정하는 setter
+	void setStatus(int stat, int amount);
+
+	void setStatus(int stst[STATS]) ;
+	//status값이 유효한지 검사하여 유효한 값이면 해당 값을 반환하고, 유효하지 않으면 기본값을 반환하는 함수
+	static int validStatValue(int stat);
 };
