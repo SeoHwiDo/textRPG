886500ded32ed44b3aa4c2fb6af49216a87acc45
user <user@DESKTOP-CTA5QIP>
2026-07-24 14:02:00 +0900

    [mod]골드 변수 추가 및 모든 변수를 unsigned로 선언해서 음수값 방지

--- textRPG/Actor.cpp
+++ textRPG/Actor.cpp
@@ -1,4 +1,4 @@
-﻿#include"Actor.h"
+#include"Actor.h"
 #include"Player.h"
 
 Actor::Actor(std::string _name) :name(_name) {
@@ -7,6 +7,7 @@ Actor::Actor(std::string _name) :name(_name) {
 	this->critical = DCRIT;
 	this->lv = DLV;
 	this->exp = DEXP;
+	this->gold = DGOLD;
 }
 Actor::~Actor() {}
 
@@ -14,17 +15,22 @@ Actor::~Actor() {}
 std::string Actor::getName() const{ return this->name; }
 void Actor::setName(std::string& _name) { this->name = _name; }
 
-int Actor::getHp() const { return this->hp; }
+unsigned int Actor::getHp() const { return this->hp; }
 void Actor::setHp(int _hp) { this->hp = _hp; }
 
-int Actor::getPower() const { return this->power; }
+unsigned int Actor::getPower() const { return this->power; }
 void Actor::setPower(int _power) { this->power = _power; }
 
-int Actor::getCritical() const { return this->critical; }
-void Actor::setCritical(int _critical) { this->critical = _critical;}
+unsigned int Actor::getCritical() const { return this->critical; }
+void Actor::setCritical(int _critical) { this->critical = _critical; }
 
-int Actor::getLv() const { return this->lv; }
+unsigned int Actor::getLv() const { return this->lv; }
 void Actor::setLv(int _lv) { this->lv = _lv; }
 
-int Actor::getExp() const { return this->exp; }
+unsigned int Actor::getExp() const { return this->exp; }
 void Actor::setExp(int _exp) { this->exp = _exp; }
+
+unsigned int Actor::getGold()const { return this->gold; }
+void Actor::setGold(int _gold) { this->gold = _gold; }
+
+
--- textRPG/Actor.h
+++ textRPG/Actor.h
@@ -1,15 +1,15 @@
-﻿#pragma once
+#pragma once
 #include<string>
-const int DHP = 1, DPOW = 0, DCRIT = 0, DLV = 0, DEXP = 0;
+const unsigned int DHP = 1, DPOW = 0, DCRIT = 0, DLV = 0, DEXP = 0, DGOLD = 0;
 class Actor {
 private:
 	std::string name;//이름
-	int hp;//체력
-	int power;//공격력
-	int critical;//치명타확률
-	int lv;
-	int exp;
-
+	unsigned int hp;//체력
+	unsigned int power;//공격력
+	unsigned int critical;//치명타확률
+	unsigned int lv;
+	unsigned int exp;
+	unsigned int gold;
 public:
 	Actor(std::string _name);
 	virtual ~Actor();//상속받은 클래스들의 소멸을 위해 virtual로 선언
@@ -17,18 +17,21 @@ public:
 	std::string getName() const;
 	void setName(std::string& _name);
 
-	int getHp() const;
+	unsigned int getHp() const;
 	void setHp(int _hp);
-	
-	int getPower() const;
+
+	unsigned int getPower() const;
 	void setPower(int _power);
-	
-	int getCritical() const;
+
+	unsigned int getCritical() const;
 	void setCritical(int _critical);
 
-	int getLv() const;
+	unsigned int getLv() const;
 	void setLv(int _lv);
 
-	int getExp() const;
+	unsigned int getExp() const;
 	void setExp(int _exp);
+
+	unsigned int getGold()const;
+	void setGold(int _gold);
 };
