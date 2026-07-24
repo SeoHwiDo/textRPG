eb5bc612f348cf62a196961b95c59cf059f904af
user <user@DESKTOP-CTA5QIP>
2026-07-24 09:39:43 +0900

    [mod]status 클래스 구조를 구조체로 수정
    변동성이 없는 데이터타입의 클래스 객체는 단순 구조체로 구현하는것이 직관적이고 효율적이기때문에 구조체로 변경

--- textRPG/Status.cpp
+++ textRPG/Status.cpp
@@ -1,24 +1,20 @@
 ﻿#include"Status.h"
-// 모든 status의 수치 get
-const std::vector<std::string> Status::statsStr = {"힘", "민첩", "건강", "지혜", "매력", "잔여포인트"};
-std::vector<int> Status::getStatusData() const {
-	return this->statusData;
-};
-//status명을 통해 해당 status의 수치 get
-int Status::getStatusData(int _stat) const {
-	return this->statusData[_stat];
-};
-
-//status명을 통해 해당 status의 수치를 amount만큼 set
-
-void Status::setStatusData(int _stat, int amount,bool add) {
-	if (add) {
-		this->statusData[_stat] += amount;
-	}
-	else {
-		this->statusData[_stat] = amount;
-	}
+void Status::setStatusStr(int amount) {
+	this->status.str = amount;
 }
-//void Status::setUpStatus(statusName _stats[], int size) {
-//	
-//}
+void Status::setStatusDex(int amount) {
+	this->status.dex = amount;
+}
+void Status::setStatusCon(int amount) {
+	this->status.con = amount;
+}
+void Status::setStatusWis(int amount) {
+	this->status.wis = amount;
+}
+void Status::setStatusCham(int amount) {
+	this->status.cham = amount;
+}
+void Status::setStatusRemain(int amount) {
+	this->status.remain = amount;
+}
+
--- textRPG/Status.h
+++ textRPG/Status.h
@@ -7,24 +7,32 @@ public:
 	const static int STAT_MIN = 0;//status의 최소값
 	const static int STAT_MAX = 25;//status의 최대값
 	const static int DSTAT = 10;//최초 스탯포인트 디폴트값
-	const static std::vector<std::string> statsStr;
 	struct statusData {
-		int str=0;
+		int str = 0;
 		int dex = 0;
 		int con = 0;
 		int wis = 0;
 		int cham = 0;
 		int remain = 0;
 	};
+
 private:
+	//actor객체가 가지고 있는 statusData 구조체
 	statusData status;
 public:
-	//status명을 통해 해당 status의 수치 get
-	statusData getStatusData() const { return this->status; }
-	int getStatStr()const { return this->status.str; }
+	//getter
+	int getStatusStr() const { return this->status.str; }
+	int getStatusDex() const { return this->status.dex; }
+	int getStatusCon() const { return this->status.con; }
+	int getStatusWis() const { return this->status.wis; }
+	int getStatusCham() const { return this->status.cham; }
+	int getStatusRemain() const { return this->status.remain; }
 
-	//status명을 통해 해당 status의 수치를 amount만큼 set, add에 true 전달시 +연산
-	void setStatusData();
-	//status세팅 로직. 강화할 status목록,강화할 status 개수
-	//void setUpStatus(statusName _stats[], int size); ***출력 방식을 정할때까지 보류
+	//setter
+	void setStatusStr(int amount);
+	void setStatusDex(int amount);	
+	void setStatusCon(int amount);
+	void setStatusWis(int amount);
+	void setStatusCham(int amount);
+	void setStatusRemain(int amount);
 };
