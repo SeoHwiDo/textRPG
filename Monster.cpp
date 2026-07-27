43149c316c8bde4f4e8420ef5e23258292b1289a
user <user@DESKTOP-CTA5QIP>
2026-07-27 16:49:26 +0900

    [mod]몬스터 객체의 전투시 결정을 위한 FSM 구현

--- Monster.cpp
+++ Monster.cpp
@@ -6,15 +6,26 @@ Monster::Monster(std::string _name) :Actor(_name) {
 	}
 }
 
-Monster::Monster(std::string _name,int stat[5]) :Actor(_name) {
-	status.setStatus(stat);
+Monster::Monster(std::string _name, int stats[Status::STATS - 1]) :Actor(_name) {
+	status.setStatus(stats);
 }
 Monster::~Monster() {}
 
 
-bool Monster::getIsEnemy() {
+bool Monster::getIsEnemy() const {
 	return this->isEnemy;
 }
 void Monster::setIsEnemy(bool _flag) {
 	this->isEnemy = _flag;
+}
+
+Monster::AIState Monster::monsterFSM(Player& _player){
+	if (_player.getDefend() >= this->power*2) {//때려도 데미지 0 -> 도망 시도
+		return RunOut;
+	}
+	else if (_player.getPower() - this->defend >= this->hp&&) {//포션 섭취
+		return Potion;
+	}
+	else if()//스킬 사용
+	else //일반공격
 }
--- Monster.h
+++ Monster.h
@@ -1,14 +1,24 @@
 ﻿#pragma once
 #include"Actor.h"
+#include"Player.h"
 class Monster :public Actor {
+
 private:
 	bool isEnemy = true;//현재 적대상태인지 확인
+
 public:
+	enum AIState {
+		Attack,
+		Skill,
+		Potion,
+		RunOut
+	};
 	Monster(std::string _name);
 	Monster(std::string _name,  int stats[Status::STATS-1]);
 	~Monster();
 
-	bool getIsEnemy();
+	bool getIsEnemy() const;
 	void setIsEnemy(bool _flag);
-
+	//몬스터의 현재 상태에 따른 다음 행동
+	AIState monsterFSM(Player& _player);
 };
