64bf79fc64e72d359bea98e2495f9a1c980a2307
user <user@DESKTOP-CTA5QIP>
2026-07-27 17:42:57 +0900

    [add]배틀 로직 추가

---
+++ Battle.cpp
@@ -0,0 +1,103 @@
+#include "Battle.h"
+#include"Util.h"
+bool Battle::crticalCheck(Actor& _actor)
+{
+	bool isCritical=Util::check_success(_actor.getCritical());
+	return isCritical;
+
+}
+bool Battle::missCheck(Actor& _attacker, Actor& _defender)
+{
+	bool isMiss = Util::check_versus_success(_attacker.status.getStatusDex(), _defender.status.getStatusDex());
+	return isMiss;
+}
+int Battle::DamageCalculation(Actor& _attacker, Actor& _defender)
+{
+	//크리티컬 판단해서 공격데미지 계산
+	int attackPower = crticalCheck(_attacker) ? _attacker.getPower() * 2 : _attacker.getPower();
+	return attackPower;
+}
+bool Battle::doRunOut(Actor& _attacker, Actor& _defender)
+{
+	bool isRun = Util::check_versus_success(_attacker.status.getStatusDex(), _defender.status.getStatusDex());
+	return isRun;
+}
+
+void Battle::doAttack(Actor& _attacker, Actor& _defender)
+{
+	int defenderPrevHp = _defender.getHp();
+	int totalDamage = std::max(0,DamageCalculation(_attacker, _defender) - _defender.getDefend());
+	_defender.setHp(defenderPrevHp-totalDamage);
+	
+}
+
+void Battle::doSkill(Actor& _attacker, Actor& _defender)
+{
+	//스킬 관련 json또는 클래스,스트럭트를 만들어서 관리
+}
+
+void Battle::doPotion(Actor& _actor, PotionType _potion)
+{
+	if (_actor.isPotionEmpty(_potion)) {
+		return;
+	}else{
+		switch (_potion)
+		{
+		case HP:
+			int prevHp = _actor.getHp();
+			_actor.setHp(prevHp + _actor.PotionSlotgetPotion(HP).potion->amount);
+			_actor.addPotion(HP,-1);
+			break;
+		case MP:
+			int prevMp = _actor.getMp();
+			_actor.setMp(prevMp + _actor.PotionSlotgetPotion(MP).potion->amount);
+			_actor.addPotion(MP, -1);
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+void Battle::doMonsterTurn()
+{
+	switch (_monster.getMonsterFSM(_player))
+	{
+	case Monster::Attack:
+		doAttack(_monster, _player);
+		break;
+	case Monster::Skill:
+		doSkill(_monster, _player);
+	case Monster::RunOut:
+		doRunOut(_monster, _player);
+	default:
+		break;
+	}
+}
+
+void Battle::doPlayerTurn(const int choice)
+{
+	switch (choice)
+	{
+	case 1:
+		doAttack(_player,_monster);
+		break;
+	case 2:
+		doSkill(_player, _monster);
+	case 3:
+		doRunOut(_player, _monster);
+	default:
+		break;
+	}
+}
+
+void Battle::judgeBattleResult(){
+	if (_player.isAlive()) {
+		_player.
+	}
+}
+
+
+
+
+
---
+++ Battle.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "Player.h"
+#include"Monster.h"
+#include"Actor.h"
+#include<memory>
+//전투 관련 함수들을 모아놓은 네임스페이스
+
+class Battle
+{
+private:
+	Monster& _monster;
+	Player& _player;
+	//Actor& _attacker;
+	//Actor& _defender;
+	//Actor& firstAttack;
+public:
+
+	int DamageCalculation(Actor& _attacker, Actor& _defender);
+
+	bool missCheck(Actor& _attacker, Actor& _defender);
+
+	bool crticalCheck(Actor& _actor);
+
+	//도망치기 사용
+	bool doRunOut(Actor& _attacker, Actor& _defender);
+	//공격 사용
+	void doAttack(Actor& _attacker, Actor& _defender);
+	//기술 사용
+	void doSkill(Actor& _attacker, Actor& _defender);
+	//포션 사용
+	void doPotion(Actor& _actor, PotionType _potion);
+
+	////공수교체
+	//void turnSwap();
+	//몬스터 행동
+	void doMonsterTurn();
+	//플레이어가 선택한 플레이어행동
+	void doPlayerTurn(const int choice);
+	//전투 결과
+	void judgeBattleResult();
+};
+
