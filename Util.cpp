c803b8c47aec8772585a8656149d2b1dcea752f0
user <user@DESKTOP-CTA5QIP>
2026-07-27 10:14:28 +0900

    [mod]정규화 없이 두 객체의 status값을 비교해서 처리

--- Util.cpp
+++ Util.cpp
@@ -3,16 +3,16 @@
 
 
 bool Util::check_versus_success(const int _myPercent, const int _enemyPercent) {
-	double myRegulaizedPercent = std::max(0.0, std::min(1.0, static_cast<double>(_myPercent) / 100.0));
-    double enemyRegulaizedPercent = std::max(0.0, std::min(1.0, static_cast<double>(_enemyPercent) / 100.0));
+	/*double myRegulaizedPercent = std::max(0.0, std::min(1.0, static_cast<double>(_myPercent) / 100.0));
+    double enemyRegulaizedPercent = std::max(0.0, std::min(1.0, static_cast<double>(_enemyPercent) / 100.0));*/
 
 	// 두 stat이 모두 0 이하이면 성공률을 계산할 수 없으므로 false 반환
-        if (_myPercent <= 0.0 && enemyRegulaizedPercent <= 0.0) return false;
+        if (_myPercent <= 0.0 && _enemyPercent <= 0.0) return false;
 
     double success_rate;
-    if (enemyRegulaizedPercent <= 0.0) success_rate = 1.0; // 상대방의 stat이 0 이하이면 무조건 성공
-    else if (myRegulaizedPercent <= 0.0) success_rate = 0.0;  // 내 stat이 0 이하이면 무조건 실패
-    else success_rate = myRegulaizedPercent / (myRegulaizedPercent + enemyRegulaizedPercent);
+    if (_enemyPercent <= 0.0) success_rate = 1.0; // 상대방의 stat이 0 이하이면 무조건 성공
+    else if (_myPercent <= 0.0) success_rate = 0.0;  // 내 stat이 0 이하이면 무조건 실패
+    else success_rate = static_cast<double>(_myPercent / (_myPercent + _enemyPercent));
 
 	success_rate = std::max(0.0, std::min(1.0, success_rate)); // 성공률을 0과 1 사이로 제한
 
