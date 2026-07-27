c81e89ce7194060562e87afee36fdaa3e924835d
user <user@DESKTOP-CTA5QIP>
2026-07-27 14:25:14 +0900

    [mod]getter 최소값 보장

--- Actor.cpp
+++ Actor.cpp
@@ -10,26 +10,26 @@ std::string Actor::getName() const { return this->name; }
 void Actor::setName(const std::string& _name) { this->name = _name; }
 
 int Actor::getHp() const { return this->hp; }
-void Actor::setHp(const int _hp) { this->hp = _hp; }
+void Actor::setHp(const int _hp) { this->hp = std::max(HP_MIN, _hp); }
 
 int Actor::getPower() const { return this->power; }
-void Actor::setPower(const int _power) { this->power = _power; }
+void Actor::setPower(const int _power) { this->power = std::max(0, _power); }
 
 int Actor::getDefend() const { return this->defend; }
-void Actor::setDefend(const int _defend) { this->defend = _defend; }
+void Actor::setDefend(const int _defend) { this->defend = std::max(0, _defend); }
 
 
 int Actor::getCritical() const { return this->critical; }
-void Actor::setCritical(const int _critical) { this->critical = _critical; }
+void Actor::setCritical(const int _critical) { this->critical = std::max(0, _critical); }
 
 int Actor::getLv() const { return this->lv; }
-void Actor::setLv(const int _lv) { this->lv = _lv; }
+void Actor::setLv(const int _lv) { this->lv = std::max(0, _lv); }
 
 int Actor::getExp() const { return this->exp; }
-void Actor::setExp(const int _exp) { this->exp = _exp; }
+void Actor::setExp(const int _exp) { this->exp = std::max(0, _exp); }
 
 int Actor::getGold()const { return this->gold; }
-void Actor::setGold(const int _gold) { this->gold = _gold; }
+void Actor::setGold(const int _gold) { this->gold = std::max(0, _gold); }
 
 
 
