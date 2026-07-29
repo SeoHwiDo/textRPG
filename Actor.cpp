cea32b901c2bfd15bdf59472f30546546f7b34c3
user <user@DESKTOP-CTA5QIP>
2026-07-29 11:48:59 +0900

    각 타입enum을 class로 변경함에 따라 int로 교차 적용이 불가, 정적 배열을 map으로 변환하여 type으로 검색 가능하도록 수정

--- Actor.cpp
+++ Actor.cpp
@@ -1,7 +1,7 @@
 ﻿#include"Actor.h"
 #include"Player.h"
 
-Actor::Actor() :name("noname"),hp(HP_MIN),mp(MP_MIN), power(POWER_MIN), defend(DEFEND_MIN), critical(CRITICAL_MIN), lv(LV_MIN), exp(EXP_MIN), gold(GOLD_MIN) {}
+Actor::Actor() :name("noname"), hp(HP_MIN), mp(MP_MIN), power(POWER_MIN), defend(DEFEND_MIN), critical(CRITICAL_MIN), lv(LV_MIN), exp(EXP_MIN), gold(GOLD_MIN) {}
 Actor::Actor(std::string _name) :name(_name), hp(HP_MIN), mp(MP_MIN), power(POWER_MIN), defend(DEFEND_MIN), critical(CRITICAL_MIN), lv(LV_MIN), exp(EXP_MIN), gold(GOLD_MIN) {}
 Actor::~Actor() {}
 
@@ -34,27 +34,36 @@ void Actor::setExp(const int _exp) { this->exp = std::max(0, _exp); }
 int Actor::getGold()const { return this->gold; }
 void Actor::setGold(const int _gold) { this->gold = std::max(0, _gold); }
 
-Actor::PotionSlot Actor::getPotion(int type) const
-{
-	return potionSlot[type];
+Actor::PotionSlot Actor::getPotion(PotionType type) const {
+	auto it = potionSlot.find(type);
+	if (it != potionSlot.end())
+		return it->second;
+	else return { nullptr,0 };
 }
 
-int Actor::getPotionNum(int type) const
-{
-	return potionSlot[type].num;
+int Actor::getPotionNum(PotionType type) const {
+	auto it = potionSlot.find(type);
+	if (it != potionSlot.end())
+		return it->second.num;
+	else return 0;
 
 }
 
-void Actor::addPotion(int type, int num){
-	potionSlot[type].num += num;
-}
-void Actor::setPotion(int code){
-	potionSlot[code/10].potion = Potion::getItemData(code);
+void Actor::addPotion(PotionType type, int num) {
+	auto it = potionSlot.find(type);
+	if (it != potionSlot.end())
+		it->second.num += num;
+	else return;
 }
 
+void Actor::setPotion(int code) {
+	auto newPotion = Potion::getItemData(code);
+	potionSlot[newPotion->type].potion = newPotion;
+}
 
-bool Actor::isPotionEmpty(int type) const{
-	if (potionSlot[type].potion == nullptr || potionSlot[type].num <= 0) {
+bool Actor::isPotionEmpty(PotionType type) const {
+	auto it = potionSlot.find(type);
+	if (it != potionSlot.end() || it->second.num <= 0) {
 		//잔여가 없음
 		return true;
 	}
@@ -62,28 +71,51 @@ bool Actor::isPotionEmpty(int type) const{
 		return false;
 }
 
-Actor::EquipSlot Actor::getEquipment(int type) const
+Actor::EquipSlot Actor::getEquipment(EquipType type) const
 {
-	return equipSlot[type];
+	auto it = equipSlot.find(type);
+	if (it != equipSlot.end())
+		return it->second;
+	else return { nullptr,0,0 };
 }
 
-void Actor::setEquipment(int code){
-	equipSlot[code / 10].equip = Equipment::getItemData(code);
+std::map<EquipType, Actor::EquipSlot> Actor::getEquipmentList() const
+{
+	return equipSlot;
+}
+
+void Actor::setEquipment(int code) {
+	auto basicEqiup = Equipment::getItemData(code);//새로운 장비로 교체시 기존 강화내용 초기화
+	if (basicEqiup != nullptr) {
+		EquipSlot newEquip;
+		newEquip.equip = basicEqiup;
+		newEquip.lv = 0;
+		newEquip.stat = 0;
+		equipSlot[basicEqiup->type] = newEquip;
+		initEquipSlot();
+	}
+	
 }
 void Actor::setEquipment(Actor::EquipSlot newEquip) {
-		equipSlot[newEquip.equip.get()->type]= newEquip;
+	equipSlot[newEquip.equip->type] = newEquip;
 }
 
 bool Actor::isEquipmentEmpty(EquipType type) const
 {
-	return equipSlot[type].equip == nullptr;
-}
-bool Actor::isEquipmentEmpty(int type) const
-{
-	return equipSlot[type].equip == nullptr;
+	auto it = equipSlot.find(type);
+	if (it == equipSlot.end() || it->second.equip == nullptr) {
+		return true;
+	}
+	return false;
 }
 void Actor::initEquipSlot() {
-	equipSlot[WEAPON].stat = equipSlot[WEAPON].equip.get()->baseStat;
+	equipSlot[EquipType::SWORD].stat = equipSlot[EquipType::SWORD].equip->baseStat + (2 << equipSlot[EquipType::SWORD].lv) * 10;//기본 무기 스탯+레벨*10
+	this->power += equipSlot[EquipType::SWORD].stat;
+}
+
+std::vector<Actor::SkillSlot> Actor::getSkillList() const
+{
+	return skillSlot;
 }
 
 
