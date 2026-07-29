88d8b40deb5d571c5afd7b1a97b7e0a7f3080819
shd <masterseo01@naver.com>
2026-07-29 23:34:32 +0900

    [mod]불필요한 include 제거

--- GameMain.cpp
+++ GameMain.cpp
@@ -1,13 +1,15 @@
 ﻿#include<iostream>
 #include<string>
-#include"Player.h"
-#include"Monster.h"
+
 #include"GameManager.h"
 
 int main() {
 	Player player = Player();
 	GameManager gm = GameManager(player);
+	Event event;
 	gm.initPlayer();
 	gm.drawFrame();
 	gm.topInfo();
+
+	gm.showEventMid(event.getEventData(Util::makeEventID(EventType::Story, 1)));
 }
