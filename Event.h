4d05962b013ca0a688b059586453c114b5ea8ec7
shd <masterseo01@naver.com>
2026-07-29 21:44:02 +0900

    [add]이벤트 클래스 구현 및 이벤트 데이터 주입

---
+++ Event.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+#include<string>
+#include<vector>
+#include<map>
+#include<memory>
+#include"Util.h"
+enum class EventAction//선택지에 따른 후속행동
+{
+	None,
+	Battle,
+	Shop,
+	Rest,
+	GameOver
+};
+
+struct EventResult//결과정보
+{
+	int hp = 0;
+	int mp = 0;
+	int exp = 0;
+	int gold = 0;
+	EventAction action = EventAction::None;
+};
+
+struct EventChoice//선택지에 따른 결과
+{
+	std::string text;
+	EventResult result;
+};
+
+struct EventData//이벤트 정보
+{
+	EventType type;
+	std::string title;
+	std::vector<std::string> description;
+	std::vector<EventChoice> choices;
+};
+class Event
+{
+public:
+	static void initDB();
+	const std::shared_ptr<EventData>& getEventData(int eventId);
+	const std::shared_ptr<EventData>& getRandomEventData(EventType type);
+protected:
+	static std::map<int,std::shared_ptr<EventData>> eventDB;
+};
+
