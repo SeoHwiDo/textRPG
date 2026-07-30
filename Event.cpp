5d34b3a9a6dfc96eb19714962eee7e8f42d603c4
user <user@DESKTOP-CTA5QIP>
2026-07-30 14:23:12 +0900

    [mod] 직전 이벤트 바로 찾아오지 않도록 수정

--- Event.cpp
+++ Event.cpp
@@ -1,6 +1,7 @@
 ﻿#include "Event.h"
 
 using namespace Util;
+#include "Monster.h"
 
 namespace {
 	std::shared_ptr<EventData> selectRandomEvent(
@@ -23,32 +24,37 @@ void Event::initDB() {
 	   EventType::Story, "모험을 떠나세요",
 	   { "당신은 신입 모험가입니다.", "이름과 스테이터스를 설정하고 ","모험을 떠나세요." },
 		{ { "내가 누구냐면...", { 0, 0, 0, 0, EventAction::None }}},
+		makeEventID(EventType::Story, 0)
 	};
 	eventDB[makeEventID(EventType::Story, 0)] = tutorial;
+
 	std::shared_ptr<EventData> story1 = std::make_shared<EventData>();
 	*story1 = {
 		EventType::Story, "안개 속의 갈림길",
-		{ "짙은 안개가 길을 삼키고 있다.", "왼쪽에서는 검은 연기가, 오른쪽에서는 희미한 종소리가 들린다." },
-		{ { "연기를 따라간다", { -5, 0, 15, 0, EventAction::Battle } },
-		  { "종소리를 따라간다", { 0, 0, 10, 5, EventAction::None } } }
+		{ "짙은 안개가 길을 삼키고 있다.", "왼쪽에서는 검은 연기가,"," 오른쪽에서는 희미한 종소리가 들린다." },
+		{ { "연기를 따라간다", { -5, 0, 15, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::Goblin) } } },
+		  { "종소리를 따라간다", { 0, 0, 10, 5, EventAction::None } } },
+		 makeEventID(EventType::Story, 1)
 	};
 	eventDB[makeEventID(EventType::Story, 1)] = story1;
 
 	std::shared_ptr<EventData> story2 = std::make_shared<EventData>();
 	*story2 = {
 		EventType::Story, "수상한 여행자",
-		{ "낡은 망토를 두른 여행자가 길을 막아선다.", "그는 물 한 모금과 정보를 바꾸자고 제안한다." },
+		{ "낡은 망토를 두른 여행자가 길을 막아선다.", "그는 물 한 모금과 ","정보를 바꾸자고 제안한다." },
 		{ { "물을 건넨다", { 5, -3, 20, 0, EventAction::None } },
-		  { "거절하고 지나간다", { 0, 0, 0, 0, EventAction::None } } }
+		  { "거절하고 지나간다", { 0, 0, 0, 0, EventAction::None } } },
+		  makeEventID(EventType::Story, 2)
 	};
 	eventDB[makeEventID(EventType::Story, 2)] = story2;
 
 	std::shared_ptr<EventData> story3 = std::make_shared<EventData>();
 	*story3 = {
 		EventType::Story, "부서진 비석",
-		{ "이름이 지워진 비석 앞에 오래된 검이 놓여 있다.", "검을 뽑자 땅속에서 낮은 진동이 전해진다." },
-		{ { "검을 뽑는다", { -10, 0, 30, 0, EventAction::Battle } },
-		  { "조용히 기도한다", { 10, 5, 10, 0, EventAction::None } } }
+		{ "이름이 지워진 비석 앞에 ","오래된 검이 놓여 있다.", "검을 뽑자 땅속에서 낮은 진동이 전해진다." },
+		{ { "검을 뽑는다", { -10, 0, 30, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::CursedArmor) } } },
+		  { "조용히 기도한다", { 10, 5, 10, 0, EventAction::None } } },
+		  makeEventID(EventType::Story, 3)
 	};
 	eventDB[makeEventID(EventType::Story, 3)] = story3;
 
@@ -56,24 +62,27 @@ void Event::initDB() {
 	std::shared_ptr<EventData> battle1 = std::make_shared<EventData>();
 	*battle1 = {
 		EventType::Battle, "굶주린 늑대", { "수풀 사이에서 굶주린 늑대가 튀어나왔다." },
-		{ { "전투를 시작한다", { 0, 0, 0, 0, EventAction::Battle } },
-		  { "금화를 던져 유인한다", { 0, 0, 0, -10, EventAction::None } } }
+		{ { "전투를 시작한다", { 0, 0, 0, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::HungryWolf) } } },
+		  { "금화를 던져 유인한다", { 0, 0, 0, -10, EventAction::None } } },
+		  makeEventID(EventType::Battle, 1)
 	};
 	eventDB[makeEventID(EventType::Battle, 1)] = battle1;
 
 	std::shared_ptr<EventData> battle2 = std::make_shared<EventData>();
 	*battle2 = {
 		EventType::Battle, "고블린 매복", { "바위 뒤에서 고블린들이 모습을 드러냈다.", "그들은 당신의 가방을 노리고 있다." },
-		{ { "정면으로 맞선다", { 0, 0, 0, 0, EventAction::Battle } },
-		  { "빠르게 도망친다", { -8, -5, 0, 0, EventAction::None } } }
+		{ { "정면으로 맞선다", { 0, 0, 0, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::Goblin), static_cast<int>(MonsterId::Goblin) } } },
+		  { "빠르게 도망친다", { -8, -5, 0, 0, EventAction::None } } },
+		makeEventID(EventType::Battle, 2)
 	};
 	eventDB[makeEventID(EventType::Battle, 2)] = battle2;
 
 	std::shared_ptr<EventData> battle3 = std::make_shared<EventData>();
 	*battle3 = {
 		EventType::Battle, "저주받은 갑옷", { "텅 빈 갑옷이 삐걱거리며 검을 들어 올린다." },
-		{ { "성스러운 문양을 확인한다", { -3, -5, 15, 0, EventAction::Battle } },
-		  { "전투 준비를 한다", { 0, 0, 0, 0, EventAction::Battle } } }
+		{ { "성스러운 문양을 확인한다", { -3, -5, 15, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::CursedArmor) } } },
+		  { "전투 준비를 한다", { 0, 0, 0, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::CursedArmor) } } } },
+		makeEventID(EventType::Battle, 3)
 	};
 	eventDB[makeEventID(EventType::Battle, 3)] = battle3;
 
@@ -82,7 +91,8 @@ void Event::initDB() {
 	*shop1 = {
 		EventType::Shop, "떠돌이 약초상", { "수레를 끄는 약초상이 회복약을 흔들어 보인다." },
 		{ { "회복약을 산다", { 20, 10, 0, -15, EventAction::Shop } },
-		  { "물건을 구경한다", { 0, 0, 0, 0, EventAction::Shop } } }
+		  { "물건을 구경한다", { 0, 0, 0, 0, EventAction::Shop } } },
+		makeEventID(EventType::Shop, 1)
 	};
 	eventDB[makeEventID(EventType::Shop, 1)] = shop1;
 
@@ -90,7 +100,8 @@ void Event::initDB() {
 	*shop2 = {
 		EventType::Shop, "대장장이의 천막", { "대장장이는 낡은 장비를 보더니 고개를 젓는다." },
 		{ { "장비를 손질한다", { 0, 0, 10, -25, EventAction::Shop } },
-		  { "인사를 하고 떠난다", { 0, 0, 0, 0, EventAction::None } } }
+		  { "인사를 하고 떠난다", { 0, 0, 0, 0, EventAction::None } } },
+		makeEventID(EventType::Shop, 2)
 	};
 	eventDB[makeEventID(EventType::Shop, 2)] = shop2;
 
@@ -98,7 +109,8 @@ void Event::initDB() {
 	*shop3 = {
 		EventType::Shop, "달빛 잡화점", { "달빛 아래에만 열린다는 작은 잡화점이다.", "주인은 지도 조각을 조심스럽게 꺼낸다." },
 		{ { "지도 조각을 산다", { 0, 0, 25, -30, EventAction::Shop } },
-		  { "다음에 사기로 한다", { 0, 0, 0, 0, EventAction::None } } }
+		  { "다음에 사기로 한다", { 0, 0, 0, 0, EventAction::None } } },
+		makeEventID(EventType::Shop, 3)
 	};
 	eventDB[makeEventID(EventType::Shop, 3)] = shop3;
 
@@ -107,7 +119,8 @@ void Event::initDB() {
 	*rest1 = {
 		EventType::Rest, "모닥불", { "누군가 남긴 모닥불이 아직 따뜻하게 타고 있다." },
 		{ { "잠시 쉰다", { 25, 15, 0, 0, EventAction::Rest } },
-		  { "불을 끄고 떠난다", { 0, 0, 0, 0, EventAction::None } } }
+		  { "불을 끄고 떠난다", { 0, 0, 0, 0, EventAction::None } } },
+		makeEventID(EventType::Rest, 1)
 	};
 	eventDB[makeEventID(EventType::Rest, 1)] = rest1;
 
@@ -115,7 +128,8 @@ void Event::initDB() {
 	*rest2 = {
 		EventType::Rest, "맑은 샘", { "바위틈에서 맑은 샘물이 흘러나온다." },
 		{ { "물을 마신다", { 15, 20, 0, 0, EventAction::Rest } },
-		  { "물통만 채운다", { 5, 5, 0, 0, EventAction::None } } }
+		  { "물통만 채운다", { 5, 5, 0, 0, EventAction::None } } },
+		makeEventID(EventType::Rest, 2)
 	};
 	eventDB[makeEventID(EventType::Rest, 2)] = rest2;
 
@@ -123,7 +137,8 @@ void Event::initDB() {
 	*rest3 = {
 		EventType::Rest, "버려진 오두막", { "낡은 오두막 안에는 먼지 쌓인 침대가 남아 있다." },
 		{ { "하룻밤 묵는다", { 35, 25, 0, 0, EventAction::Rest } },
-		  { "주변을 경계하며 쉰다", { 15, 10, 0, 0, EventAction::Rest } } }
+		  { "주변을 경계하며 쉰다", { 15, 10, 0, 0, EventAction::Rest } } },
+		makeEventID(EventType::Rest, 3)
 	};
 	eventDB[makeEventID(EventType::Rest, 3)] = rest3;
 
@@ -132,7 +147,8 @@ void Event::initDB() {
 	*treasure1 = {
 		EventType::Treasure, "이끼 낀 상자", { "돌무더기 사이에서 작은 나무 상자를 발견했다." },
 		{ { "조심스럽게 연다", { 0, 0, 10, 35, EventAction::None } },
-		  { "함정을 확인한다", { 0, -5, 20, 20, EventAction::None } } }
+		  { "함정을 확인한다", { 0, -5, 20, 20, EventAction::None } } },
+		makeEventID(EventType::Treasure, 1)
 	};
 	eventDB[makeEventID(EventType::Treasure, 1)] = treasure1;
 
@@ -140,7 +156,8 @@ void Event::initDB() {
 	*treasure2 = {
 		EventType::Treasure, "침몰한 수레", { "진흙에 반쯤 잠긴 상인의 수레가 보인다." },
 		{ { "짐칸을 뒤진다", { -5, 0, 15, 50, EventAction::None } },
-		  { "바퀴 아래를 살핀다", { 0, 0, 25, 20, EventAction::None } } }
+		  { "바퀴 아래를 살핀다", { 0, 0, 25, 20, EventAction::None } } },
+		makeEventID(EventType::Treasure, 2)
 	};
 	eventDB[makeEventID(EventType::Treasure, 2)] = treasure2;
 
@@ -148,7 +165,8 @@ void Event::initDB() {
 	*treasure3 = {
 		EventType::Treasure, "별빛 광맥", { "동굴 벽에 별빛처럼 반짝이는 광맥이 드러나 있다." },
 		{ { "광석을 캔다", { -10, -10, 25, 60, EventAction::None } },
-		  { "작은 조각만 챙긴다", { 0, -3, 10, 20, EventAction::None } } }
+		  { "작은 조각만 챙긴다", { 0, -3, 10, 20, EventAction::None } } },
+		makeEventID(EventType::Treasure, 3)
 	};
 	eventDB[makeEventID(EventType::Treasure, 3)] = treasure3;
 
@@ -156,30 +174,34 @@ void Event::initDB() {
 	std::shared_ptr<EventData> boss1 = std::make_shared<EventData>();
 	*boss1 = {
 		EventType::Boss, "숲의 수호자", { "고목이 뿌리를 일으켜 길을 막아선다.", "숲의 수호자는 침입자를 용서하지 않는다." },
-		{ { "수호자에게 도전한다", { 0, 0, 0, 0, EventAction::Battle } },
-		  { "숲에 사과한다", { -5, -10, 10, 0, EventAction::Battle } } }
+		{ { "수호자에게 도전한다", { 0, 0, 0, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::ForestGuardian) } } },
+		  { "숲에 사과한다", { -5, -10, 10, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::ForestGuardian) } } } },
+		makeEventID(EventType::Boss, 1)
 	};
 	eventDB[makeEventID(EventType::Boss, 1)] = boss1;
 
 	std::shared_ptr<EventData> boss2 = std::make_shared<EventData>();
 	*boss2 = {
 		EventType::Boss, "붉은 달의 기사", { "붉은 달빛 아래, 검은 기사가 결투를 청한다.", "그의 검에서는 불길한 기운이 새어 나온다." },
-		{ { "정정당당히 결투한다", { 0, 0, 0, 0, EventAction::Battle } },
-		  { "선제 공격을 시도한다", { -10, -5, 10, 0, EventAction::Battle } } }
+		{ { "정정당당히 결투한다", { 0, 0, 0, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::RedMoonKnight) } } },
+		  { "선제 공격을 시도한다", { -10, -5, 10, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::RedMoonKnight) } } } },
+		makeEventID(EventType::Boss, 2)
 	};
 	eventDB[makeEventID(EventType::Boss, 2)] = boss2;
 
 	std::shared_ptr<EventData> boss3 = std::make_shared<EventData>();
 	*boss3 = {
 		EventType::Boss, "심연의 군주", { "깊은 균열에서 심연의 군주가 모습을 드러낸다.", "이 싸움의 끝이 여정의 운명을 결정할 것이다." },
-		{ { "최후의 전투를 시작한다", { 0, 0, 0, 0, EventAction::Battle } },
-		  { "마력을 모두 끌어낸다", { -15, -30, 20, 0, EventAction::Battle } } }
+		{ { "최후의 전투를 시작한다", { 0, 0, 0, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::AbyssLord) } } },
+		  { "마력을 모두 끌어낸다", { -15, -30, 20, 0, EventAction::Battle, -1, { static_cast<int>(MonsterId::AbyssLord) } } } },
+		makeEventID(EventType::Boss, 3)
 	};
 	eventDB[makeEventID(EventType::Boss, 3)] = boss3;
 	std::shared_ptr<EventData> Empty999 = std::make_shared<EventData>();
 	*Empty999 = {
 		EventType::Empty,"잘못된EventID",{"존재하지 않는 EventID를 호출했습니다.","이벤트 호출 위치를 확인하세요."},
-		{}
+		{},
+		makeEventID(EventType::Empty, 0)
 	};
 	eventDB[makeEventID(EventType::Empty, 0)] = Empty999;
 }
@@ -189,25 +211,25 @@ const std::shared_ptr<EventData>& Event::getEventData(int eventId)
 	return (eventDB.find(eventId) != eventDB.end()) ? eventDB[eventId] : eventDB[makeEventID(EventType::Empty, 0)];
 }
 //전체 랜덤 호출
-std::shared_ptr<EventData> Event::getRandomEventData()
+std::shared_ptr<EventData> Event::getRandomEventData(int tmpId)
 {
 	//벡터에 유효한 이벤트만 담아서 해당 벡터의 인덱스범위중 하나 랜덤 호출
 	std::vector<std::shared_ptr<EventData>> candidates;
 	for (const auto& entry : eventDB) {
 		const auto& eventData = entry.second;
-		if (eventData && eventData->type != EventType::Empty) {
+		if (eventData && eventData->type != EventType::Empty && entry.first != tmpId) {
 			candidates.push_back(eventData);
 		}
 	}
 
 	return selectRandomEvent(candidates, getEventData(makeEventID(EventType::Empty, 0)));
 }
-std::shared_ptr<EventData> Event::getRandomEventData(EventType type)
+std::shared_ptr<EventData> Event::getRandomEventData(int tmpId,EventType type)
 {
 	std::vector<std::shared_ptr<EventData>> candidates;
 	for (const auto& entry : eventDB) {
 		const auto& eventData = entry.second;
-		if (eventData && eventData->type == type) {
+		if (eventData && eventData->type == type && entry.first != tmpId) {
 			candidates.push_back(eventData);
 		}
 	}
@@ -215,13 +237,13 @@ std::shared_ptr<EventData> Event::getRandomEventData(EventType type)
 	return selectRandomEvent(candidates, getEventData(makeEventID(EventType::Empty, 0)));
 }
 
-std::shared_ptr<EventData> Event::getRandomEventData(std::initializer_list<EventType> types)
+std::shared_ptr<EventData> Event::getRandomEventData(int tmpId,std::initializer_list<EventType> types)
 {
 	std::vector<std::shared_ptr<EventData>> candidates;
 	for (auto type : types) {
 		for (const auto& entry : eventDB) {
 			const auto& eventData = entry.second;
-			if (eventData && eventData->type == type) {
+			if (eventData && eventData->type == type&&entry.first!=tmpId) {
 				candidates.push_back(eventData);
 			}
 		}
--- Event.h
+++ Event.h
@@ -21,6 +21,7 @@ struct EventResult//결과정보
 	int gold = 0;
 	EventAction thisAction = EventAction::None;
 	int nextEvent=-1;//만약 연계 이벤트 존재시 해당 이벤트 유니크넘버, 없으면 -1
+	std::vector<int> monsterIds;//전투 이벤트에서 소환할 몬스터 DB ID 목록
 
 };
 
@@ -36,15 +37,16 @@ struct EventData//이벤트 정보
 	std::string title;
 	std::vector<std::string> description;
 	std::vector<EventChoice> choices;
+	int id;
 };
 class Event
 {
 public:
 	static void initDB();
 	const std::shared_ptr<EventData>& getEventData(int eventId);
-	std::shared_ptr<EventData> getRandomEventData();
-	std::shared_ptr<EventData> getRandomEventData(EventType type);
-	std::shared_ptr<EventData> getRandomEventData(std::initializer_list<EventType> types);
+	std::shared_ptr<EventData> getRandomEventData(int tmpId);
+	std::shared_ptr<EventData> getRandomEventData(int tmpId,EventType type);
+	std::shared_ptr<EventData> getRandomEventData(int tmpId,std::initializer_list<EventType> types);
 protected:
 	static std::map<int,std::shared_ptr<EventData>> eventDB;
 };
