75e0dfbb9cdb715053228939692157e9fd85637a
shd <masterseo01@naver.com>
2026-07-30 23:56:56 +0900

    매개변수 수정

--- Util.cpp
+++ Util.cpp
@@ -2,17 +2,17 @@
 
 
 
-bool Util::check_versus_success(const int _myPercent, const int _enemyPercent) {
+bool Util::check_versus_success(const int attacker, const int defender) {
 	/*double myRegulaizedPercent = std::max(0.0, std::min(1.0, static_cast<double>(_myPercent) / 100.0));
     double enemyRegulaizedPercent = std::max(0.0, std::min(1.0, static_cast<double>(_enemyPercent) / 100.0));*/
 
 	// 두 stat이 모두 0 이하이면 성공률을 계산할 수 없으므로 false 반환
-        if (_myPercent <= 0.0 && _enemyPercent <= 0.0) return false;
+        if (attacker <= 0.0 && defender <= 0.0) return false;
 
     double success_rate;
-    if (_enemyPercent <= 0.0) success_rate = 1.0; // 상대방의 stat이 0 이하이면 무조건 성공
-    else if (_myPercent <= 0.0) success_rate = 0.0;  // 내 stat이 0 이하이면 무조건 실패
-    else success_rate = static_cast<double>(_myPercent) / (_myPercent + _enemyPercent);
+    if (defender <= 0.0) success_rate = 1.0; // 상대방의 stat이 0 이하이면 무조건 성공
+    else if (attacker <= 0.0) success_rate = 0.0;  // 내 stat이 0 이하이면 무조건 실패
+    else success_rate = static_cast<double>(attacker) / (attacker + defender);
 
 	success_rate = std::max(0.0, std::min(1.0, success_rate)); // 성공률을 0과 1 사이로 제한
 
